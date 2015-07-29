f7664b420c93f1639f5642c1d70894971ee42165
Nathan Voxland <nathan@voxland.net>
2015-07-29 14:22:08 -0500

    Updated maven config and support liquibase 3.4.1

---
+++ src/test/java/liquibase/ext/mssql/sqlgenerator/InsertGeneratorTest.java
@@ -0,0 +1,21 @@
+package liquibase.ext.mssql.sqlgenerator;
+
+import liquibase.statement.core.InsertStatement;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import liquibase.sqlgenerator.SqlGeneratorFactory;
+import liquibase.database.core.MSSQLDatabase;
+import liquibase.sql.Sql;
+
+public class InsertGeneratorTest {
+    @Test
+    public void integrates() {
+        InsertStatement statement = new InsertStatement(null, null, "TABLE_NAME");
+        statement.addColumnValue("id", 1);
+        statement.addColumnValue("name", "asdf");
+
+        Sql[] sql = SqlGeneratorFactory.getInstance().generateSql(statement, new MSSQLDatabase());
+        assertEquals(3, sql.length);
+    }
+
+}
